d4282ee596a8ba0707db8e5c932afb22c4eaa237
Kraius Magnanimus <kurimao069@gmail.com>
2026-01-30 12:37:15 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Root domain deployment for mycocopure.com
+  base: '/Cocopure',  // Root domain deployment for mycocopure.com
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
