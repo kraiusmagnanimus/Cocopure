ecbcaeb996eeaf5435317b2cf27c0af5eee03197
Kraius Magnanimus <kurimao069@gmail.com>
2026-01-30 13:04:26 +0800

    Setup custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kraiusmagnanimus.github.io/Cocopure/',  // Root domain deployment for mycocopure.com
+  base: '/',  // Root domain deployment for mycocopure.com
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
